53e6340eb3a324d1aa2f9b2b9be9e32262e9e877
Sam Rose <hello@samwho.dev>
2023-08-01 11:45:51 +0100

    Add CI.

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-    testEnvironment: 'jsdom',
-    testRegex: '/tests.ts$',
-    moduleFileExtensions: ['ts', 'js'],
-};
---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  transform: { "^.+\\.ts?$": "ts-jest" },
+  testEnvironment: "jsdom",
+  testRegex: "/tests.ts$",
+  moduleFileExtensions: ["ts", "js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts"],
+};
+
+export default config;
